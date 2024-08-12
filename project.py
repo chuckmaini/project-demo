6f38bee2d8f5b56230a16bc3f2923bdde16aedcb
Chuck Maini <chuckmaini@gmail.com>
2024-08-12 13:25:37 -0400

    testing how all this works

---
+++ project.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+a = 10
+b = 20
+
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
